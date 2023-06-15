588791928baf61da65d847168343806467714d94
KotoWhiskas <kotowhiskas@protonmail.com>
2023-06-15 21:31:55 +0200

    commit....

---
+++ C#N_own/NamedPipe.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+namespace C_N_own
+{
+    class NamedPipe
+    {
+
+        public static NamedPipeClientStream pipeClient{ get; protected set; }
+
+        // Windows реализация
+        static NamedPipe()
+        {
+            Console.WriteLine("Waiting for client to connect to the pipe...");
+            pipeClient = new NamedPipeClientStream(".", "mynamedpipe", PipeDirection.InOut);
+            pipeClient.Connect();
+            Console.WriteLine("Client connected.");
+        }
+
+        public static void Write(string text)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(text + "\n");
+            pipeClient.Write(messageBytes, 0, messageBytes.Length);
+        }
+    }
+}
+
